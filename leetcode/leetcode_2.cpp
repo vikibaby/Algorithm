ae42aea0fd280c501df28f2f7212cb32ca411c52
vikibaby <394890459@qq.com>
2017-07-04 12:48:36 +0100

    Update leetcode_2.cpp

--- leetcode/leetcode_2.cpp
+++ leetcode/leetcode_2.cpp
@@ -1,6 +1,6 @@
 /**
- * Definition for singly-linked list.
- * struct ListNode {
+ * Definition for singly-linked list. //should be in headfile
+ * struct ListNode { 
  *     int val;
  *     ListNode *next;
  *     ListNode(int x) : val(x), next(NULL) {}
@@ -8,16 +8,16 @@
  */
 class Solution {
 public:
-    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* res = new ListNode(0);
-        ListNode* head = res;
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) { //return address of value of function is ListNode type
+        ListNode* res = new ListNode(0);// line6 new construction fuction
+        ListNode* head = res; // * get value from add
         int add = 0;
-        while(l1 && l2){
+        while(l1 && l2){  //logical and , 1. l1:2,l2:5
             //cout << l1->val << " " << l2->val << endl;
-            add = l1->val + l2->val + add;
-            l1->val = add % 10;
-            add = add / 10;
-            head = head->next = l1;
+            add = l1->val + l2->val + add; // -> get valure of pointer,add:7
+            l1->val = add % 10; //l1:7
+            add = add / 10; //add:0
+            head = head->next = l1; 
             l1 = l1->next;
             l2 = l2->next;
         }
@@ -28,7 +28,7 @@ public:
 
         ListNode* last = head;
         head = head->next;
-        while(head){
+        while(head){ //head 
             head->val = head->val + add;
             if(head->val < 10){
                 add = 0;
@@ -43,7 +43,7 @@ public:
         if(add == 0)
             return res->next;
         else{
-            last = last->next = res;
+            last = last->next = res; //add a single new linked list
             res->val = 1;
             res = res->next;
             last->next = NULL;
